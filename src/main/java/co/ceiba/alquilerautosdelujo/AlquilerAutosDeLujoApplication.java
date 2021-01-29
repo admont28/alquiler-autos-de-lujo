36669f45845794437da0daf215799c5f77fcd644
Andrés David Montoya Aguirre - Ceiba Software <andres.montoya@ceiba.com.co>
2021-01-29 10:46:23 -0500

    Proyecto inicial

---
+++ src/main/java/co/ceiba/alquilerautosdelujo/AlquilerAutosDeLujoApplication.java
@@ -0,0 +1,13 @@
+package co.ceiba.alquilerautosdelujo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlquilerAutosDeLujoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlquilerAutosDeLujoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/ceiba/alquilerautosdelujo/AlquilerAutosDeLujoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ceiba.alquilerautosdelujo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlquilerAutosDeLujoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
