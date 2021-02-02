c4e72459467ce3ab309c9625868fb147438340ea
Andrés David Montoya Aguirre <andres.montoya@ceiba.com.co>
2021-02-02 08:49:44 -0500

    Adición nuevamente de pruebas de integración de alquiler

---
+++ backend/microservicio/infraestructura/src/test/java/com/ceiba/alquiler/controlador/ComandoControladorAlquilerTest.java
@@ -0,0 +1,44 @@
+package com.ceiba.alquiler.controlador;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.ceiba.ApplicationMock;
+import com.ceiba.alquiler.comando.ComandoAlquiler;
+import com.ceiba.alquiler.servicio.testdatabuilder.ComandoAlquilerTestDataBuilder;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes= ApplicationMock.class)
+@WebMvcTest(ComandoControladorAlquiler.class)
+public class ComandoControladorAlquilerTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private MockMvc mocMvc;
+
+    @Test
+    public void crearAlquilerTest() throws Exception{
+        // arrange
+        ComandoAlquiler alquiler = new ComandoAlquilerTestDataBuilder().build();
+
+        // act - assert
+        mocMvc.perform(post("/alquiler")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(alquiler)))
+                .andExpect(status().isOk())
+                .andExpect(content().json("{'valor': 1}"));
+    }
+}
