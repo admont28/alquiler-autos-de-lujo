5f7bcc961e78578d7f37c079bbb486d9467a15ff
Andrés David Montoya Aguirre <andres.montoya@ceiba.com.co>
2021-02-05 18:08:57 -0500

    Mejorando covertura de código backend

--- backend/microservicio/dominio/src/test/java/com/ceiba/auto/servicio/ServicioActualizarAutoTest.java
+++ backend/microservicio/dominio/src/test/java/com/ceiba/auto/servicio/ServicioActualizarAutoTest.java
@@ -10,8 +10,22 @@ import com.ceiba.auto.modelo.entidad.Auto;
 import com.ceiba.auto.puerto.repositorio.RepositorioAuto;
 import com.ceiba.auto.servicio.testdatabuilder.AutoTestDataBuilder;
 import com.ceiba.dominio.excepcion.ExcepcionDuplicidad;
+import com.ceiba.dominio.excepcion.ExcepcionNegocio;
 
 public class ServicioActualizarAutoTest {
+	
+	@Test
+    public void validarAutoExistenciaTest() {
+        // arrange
+		Auto auto = new AutoTestDataBuilder().conId(10000L).build();
+        RepositorioAuto repositorioAuto = Mockito.mock(RepositorioAuto.class);
+        Mockito.when(repositorioAuto.buscar(Mockito.anyLong())).thenReturn(null);
+        Mockito.when(repositorioAuto.existeExcluyendoId(Mockito.anyLong(), Mockito.anyString())).thenReturn(true);
+        ServicioActualizarAuto servicioActualizarAuto = new ServicioActualizarAuto(repositorioAuto);
+        
+        // act - assert
+        BasePrueba.assertThrows(() -> servicioActualizarAuto.ejecutar(auto), ExcepcionNegocio.class,"El auto que intenta actualizar, no existe en el sistema");
+    }
 	
     @Test
     public void validarAutoExistenciaPreviaTest() {
